1f3a85eb6179ccbc81c5f9801cd1b604d9109e7b
sook-cheng.chan <sook-cheng.chan@basf.com>
2024-10-19 00:50:09 +0800

    shopping cart WIP

---
+++ src/hooks/cart-context/CartContext.tsx
@@ -0,0 +1,57 @@
+import { createContext, useContext, useReducer } from "react";
+import { products } from "../../pages/shopping-cart/ShoppingCartData";
+
+export interface ProductItem {
+  id?: number;
+  code?: string;
+  name?: string;
+  description?: string;
+  image?: string;
+  variation?: string;
+  size?: string;
+  color?: string;
+  finish?: string;
+  price?: number;
+  quantity?: number;
+}
+
+export interface ShoppingCartState {
+  products: ProductItem[];
+}
+
+const INITIAL_STATE: ShoppingCartState = {
+  products: products,
+};
+
+export const ADD_ITEM = "[CART_CONTEXT] ADD_ITEM";
+export const REMOVE_ITEM = "[CART_CONTEXT] REMOVE_ITEM";
+export const CLEAR_ALL_ITEMS = "[CART_CONTEXT] CLEAR_ALL_ITEMS";
+
+const reducer = (
+  state: ShoppingCartState,
+  action: { type: string; payload: any }
+): ShoppingCartState => {
+  const { type, payload } = action;
+  switch (type) {
+    default:
+      return state;
+  }
+};
+
+const cartContext = createContext<{
+  cartState: ShoppingCartState;
+  cartDispatch: React.Dispatch<any>;
+}>({ cartState: INITIAL_STATE, cartDispatch: () => null });
+
+export const CartProvider = ({ children }: any) => {
+  const [cartState, cartDispatch] = useReducer(reducer, INITIAL_STATE);
+  return (
+    <cartContext.Provider value={{ cartState, cartDispatch }}>
+      {children}
+    </cartContext.Provider>
+  );
+};
+
+export const useCartContext = () => {
+  return useContext(cartContext);
+};
--- src/pages/shopping-cart/ShoppingCart.tsx
+++ src/pages/shopping-cart/ShoppingCart.tsx
@@ -1,14 +1,130 @@
-import { useEffect, useState } from 'react';
+import { Sidebar } from "primereact/sidebar";
+import { DataView, DataViewLayoutOptions } from "primereact/dataview";
+import {
+  InputNumber,
+  InputNumberValueChangeEvent,
+} from "primereact/inputnumber";
+import { Dispatch, SetStateAction, useEffect, useMemo, useState } from "react";
+import {
+  ProductItem,
+  useCartContext,
+} from "../../hooks/cart-context/CartContext";
+import { Button } from "primereact/button";
+import product_A from "../../assets/images/products/product_A.jpg";
+import product_B from "../../assets/images/products/product_B.jpg";
 
-const ShoppingCart = () => {
+interface Props {
+  visible: boolean;
+  setVisible: Dispatch<SetStateAction<boolean>>;
+}
 
+const ShoppingCart = ({ visible, setVisible }: Props) => {
+  const { cartState, cartDispatch } = useCartContext();
+  //   const [visible, setVisible] = useState(false);
+  const products = useMemo(() => cartState.products, [cartState.products]);
+
+  // useEffect(() => {}, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const setQuantity = (e: InputNumberValueChangeEvent) => {
+    console.log("quantity", e.value);
+  };
+
+  const onClickRemoveItem = (item: ProductItem) => {};
+
+  const onClickInquiry = () => {
+    const phoneNumber = "+60169926846";
+    const message = "Hi, I would like to inquire...";
+    const url = `https://web.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(
+      message
+    )}&app_absent=0`;
+    window.open(url, "_blank");
+  };
+
+  const itemTemplate = (data: ProductItem, index: number) => {
+    return (
+      <div key={index}>
+        <div className="cart-item">
+          <img className="product-image" src={`${product_A}`} alt={data.name} />
+          <div className="flex flex-column lg:flex-row justify-content-between align-items-center xl:align-items-start lg:flex-1 gap-4">
+            <div className="flex flex-column gap-1">
+              <div className="text-2xl font-bold text-900">{data.name}</div>
+              <div className="text-700">{data.description}</div>
+            </div>
+            <div className="flex flex-column gap-2">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Code</td>
+                    <td>:</td>
+                    <td>{data.code}</td>
+                  </tr>
+                  <tr>
+                    <td>Variation</td>
+                    <td>:</td>
+                    <td>{data.variation}</td>
+                  </tr>
+                  <tr>
+                    <td>Size</td>
+                    <td>:</td>
+                    <td>{data.size}</td>
+                  </tr>
+                  <tr>
+                    <td>Color</td>
+                    <td>:</td>
+                    <td>{data.color}</td>
+                  </tr>
+                  <tr>
+                    <td>Finish</td>
+                    <td>:</td>
+                    <td>{data.finish}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <Button
+            link
+            label="Remove"
+            onClick={() => onClickRemoveItem(data)}
+          ></Button>
+        </div>
+      </div>
+    );
+  };
+
+  const listTemplate = (items: ProductItem[]) => {
+    if (!items || items.length === 0) return null;
+
+    let list = items.map((product, index) => {
+      return itemTemplate(product, index);
+    });
+
+    return <div className="grid grid-nogutter">{list}</div>;
+  };
 
   return (
-    <>
- 
-
-    </>
+    <Sidebar
+      visible={visible}
+      position="right"
+      header={
+        <div>
+          <h2>Cart</h2>
+        </div>
+      }
+      onHide={() => setVisible(false)}
+    >
+      <div className="card">
+        <DataView value={products} listTemplate={listTemplate} />
+      </div>
+      <Button
+        label="Inquire Our Sales Team"
+        icon="pi pi-angle-right"
+        iconPos="right"
+        onClick={onClickInquiry}
+        rounded
+      />
+    </Sidebar>
   );
-}
+};
 
 export default ShoppingCart;
