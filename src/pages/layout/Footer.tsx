d5e9fe15478c4666717d86444298c3535efac485
Sook Cheng <j.sookcheng@gmail.com>
2025-01-27 23:03:58 +0800

    change to render contact as html

--- src/pages/layout/Footer.tsx
+++ src/pages/layout/Footer.tsx
@@ -68,7 +68,6 @@ const Footer = forwardRef<HTMLDivElement>((props, ref) => {
               width > 640 ? "70px 1fr" : width <= 640 ? "90px 1fr" : "90px 1fr"
             }
             gridTemplateColumns={"1fr 1fr"}
-            h={width > 640 ? "350px" : width <= 640 ? "390px" : "390px"}
             gap="1"
           >
             {/* <GridItem pl="2" area={"logo"}>
@@ -94,7 +93,7 @@ const Footer = forwardRef<HTMLDivElement>((props, ref) => {
                 </p>
               </div>
             </GridItem>
-            <GridItem pl="15" pr="15" area={"nav"}>
+            <GridItem style={{ padding: "0 15px 50px 15px" }} area={"nav"}>
               <Box
                 style={{
                   display: "flex",
@@ -165,22 +164,7 @@ const Footer = forwardRef<HTMLDivElement>((props, ref) => {
                     </Heading>
                   </div>
                   <div className="mobile-nav-block">
-                    <div className="contact-text-block-mobile">
-                      <span>Main Branch:</span>
-                      <span>{companyInfo.find((x: any) => x.key === 'CONTACT_MAIN')?.value}</span>
-                    </div>
-                    <div className="contact-text-block-mobile">
-                      <span>Penang Branch:</span>
-                      <span>{companyInfo.find((x: any) => x.key === 'CONTACT_PENANG')?.value}</span>
-                    </div>
-                    <div className="contact-text-block-mobile">
-                      <span>Johor Branch:</span>
-                      <span>{companyInfo.find((x: any) => x.key === 'CONTACT_JOHOR')?.value}</span>
-                    </div>
-                    <div className="contact-text-block-mobile">
-                      <span>Fax:</span>
-                      <span>{companyInfo.find((x: any) => x.key === 'CONTACT_FAX')?.value}</span>
-                    </div>
+                    <div dangerouslySetInnerHTML={{ __html: companyInfo.find((x: any) => x.key === 'CONTACT_MOBILE')?.value }}></div>
                   </div>
                   <div className="mobile-social-block">
                     <IconButton
@@ -287,24 +271,7 @@ const Footer = forwardRef<HTMLDivElement>((props, ref) => {
                 </Heading>
               </div>
               <div className="contact-block">
-                <div>
-                  <span>Main Branch</span>
-                  <span>Penang Branch</span>
-                  <span>Johor Branch</span>
-                  <span>Fax</span>
-                </div>
-                <div>
-                  <span>:</span>
-                  <span>:</span>
-                  <span>:</span>
-                  <span>:</span>
-                </div>
-                <div>
-                  <span>{companyInfo.find((x: any) => x.key === 'CONTACT_MAIN')?.value}</span>
-                  <span>{companyInfo.find((x: any) => x.key === 'CONTACT_PENANG')?.value}</span>
-                  <span>{companyInfo.find((x: any) => x.key === 'CONTACT_JOHOR')?.value}</span>
-                  <span>{companyInfo.find((x: any) => x.key === 'CONTACT_FAX')?.value}</span>
-                </div>
+                <div dangerouslySetInnerHTML={{ __html: companyInfo.find((x: any) => x.key === 'CONTACT_DESKTOP')?.value }}></div>
               </div>
               <div className="social-block">
                 <IconButton
